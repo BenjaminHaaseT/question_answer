3e71d57de9cc71a0b9ec7e2bac7358c29c7e9395
Benjamin Haase <benjaminthaase@protonmail.com>
2024-01-09 11:15:23 -0800

    Adds structs to model the database in models module

---
+++ src/models/mod.rs
@@ -0,0 +1,53 @@
+//! Contains the structs that model the databases.
+
+use serde::{Serialize, Deserialize};
+use sqlx::types::Uuid;
+
+/// A new question received from a request.
+#[derive(Debug, Serialize, Deserialize)]
+pub struct NewQuestion {
+    /// The title of the new question
+    pub title: String,
+    /// The content of the new question
+    pub question: String,
+}
+
+/// A question that has been successfully persisted in the database.
+#[derive(Debug)]
+pub struct Question {
+    /// The unique id of the question
+    id: Uuid,
+    /// The title of the question
+    title: String,
+    /// The content of the question
+    question: String,
+    /// The number of likes the question has received
+    likes: u32,
+    /// The timestamp as a string the question was created
+    created_at: String,
+    // tags: Vec<Option<>>
+}
+
+/// A new answer to an associated question received from a request.
+#[derive(Debug, Serialize, Deserialize)]
+pub struct NewAnswer {
+    /// The id of the question the new answer is responding to
+    pub question_id: String,
+    /// The content of the new answer
+    pub answer: String,
+}
+
+/// An answer that has been successfully persisted in the database.
+#[derive(Debug)]
+pub struct Answer {
+    /// The unique id of the answer
+    id: Uuid,
+    /// The unique id of the associated question
+    question_id: Uuid,
+    /// The content of the answer
+    answer: String,
+    /// The number of likes the answer has received
+    likes: u32,
+    /// The timestamp the answer was created at as a string
+    created_at: String
+}
