239a9b97f08b7c2f43978356be80232b44ce3fae
Benjamin Haase <benjaminthaase@protonmail.com>
2024-01-10 11:21:23 -0800

    Adds documentation in persistence module

--- src/persistence/mod.rs
+++ src/persistence/mod.rs
@@ -54,16 +54,59 @@ pub trait QuestionDao {
 
 /// The interface for any database access object that will interact with the answers database.
 pub trait AnswerDao {
+    /// Creates a new answer for a particular question and inserts it into the database.
+    ///
+    /// # Parameters
+    /// `question_id`: The `EntityId` of the new answer is attempting to respond to
+    /// `new_answer`: The `NewAnswer` containing the content of the answer to be inserted into the database
+    ///
+    /// # Returns
+    /// A `Result<Uuid, DbError>`, if the answer was created successfully a `Ok(Uuid)` will be returned
+    /// where the `Uuid` represents the id of the newly created answer, otherwise `Err(DbError)` will be returned.
     async fn create_answer(question_id: EntityId, new_answer: NewAnswer) -> Result<Uuid, DbError>;
 
+    /// Gets an answer from the database if present
+    ///
+    /// # Parameters
+    /// `answer_id` the `EntityId` of the answer being queried
+    ///
+    ///# Returns
+    /// A `Result<Answer, DbError>`, a `Ok(Question)` if the query is successful, otherwise `Err(DbError)`.
     async fn get_answer(answer_id: EntityId) -> Result<Answer, DbError>;
 
+    /// Gets a `Vec` of all answers in the database associated with a particular question.
+    ///
+    /// # Parameters
+    /// `question_id`: The id of the `Question` whose answers are to be returned.
+    ///
+    /// # Returns
+    /// A `Result<Vec<Answer>>, DbError>`, in the success case `Ok(Vec<Answer>)`, otherwise `Err(DbError)`.
     async fn get_answers(question_id: EntityId) -> Result<Vec<Answer>, DbError>;
 
+    /// Gets a `Vec` of all answers in the database
+    ///
+    /// # Returns
+    /// A `Result<Vec<Answer>>, DbError>`, in the success case `Ok(Vec<Question>)`, otherwise `Err(DbError)`.
     async fn get_all_answers() -> Result<Vec<Answer>, DbError>;
 
-    async fn delete_answer(answer_id: EntityId) -> Result<(), DbError>;
+    /// Deletes an answer from the database.
+    ///
+    /// # Parameters
+    /// `answer_id`: The `EntityId` of the `Answer` to be deleted.
+    ///
+    /// # Returns
+    /// A `Result<Uuid, DbError>`, if the answer is successfully deleted then a `Ok(Uuid)` will be returned,
+    /// otherwise an `Err(DbError)` is returned.
+    async fn delete_answer(answer_id: EntityId) -> Result<Uuid, DbError>;
 
+    /// Increments the number of likes associated with a particular answer.
+    ///
+    /// # Parameters
+    /// `answer_id`: The `EntityId` of the `Answer` being queried
+    ///
+    /// # Returns
+    /// A `Result<(), DbError>`, `Ok(())` in the successful case and `Err(DbError)` in the
+    /// unsuccessful case.
     async fn increment_answer_likes(answer_id: EntityId) -> Result<(), DbError>;
 
 }
